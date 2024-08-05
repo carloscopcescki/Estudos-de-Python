9edd7d7915dcb5ec20013ee0d6f1860935795fe9
Carlos Copcescki <41136340+carloscopcescki@users.noreply.github.com>
2024-08-05 11:39:26 -0300

    Uso de herança de classes

--- Cap 9 - (Classes)/car.py
+++ Cap 9 - (Classes)/car.py
@@ -1,36 +1,41 @@
-class Car:
-    """Simples tentativa de representar um carro"""
-    def __init__(self, make, model, year) -> None:
+class Car():
+    def __init__(self, make, model, year):
         self.make = make
         self.model = model
         self.year = year
         self.odometer_reading = 0
-        pass
-    
-    def get_descriptive_name(self):
-        '''Nome completo do modelo do carro'''
-        long_name = f'{self.year} {self.make} {self.model}'
-        return long_name.title()
+        
+    def get_descritive_name(self):
+        long_name = print(f"{self.model} {self.make} {self.year}")
+        return long_name
     
     def read_odometer(self):
-        '''Exibir uma frase indicando kilometragem do carro'''
-        print(f"Esse carro está rodando a {self.odometer_reading}Km/h.")
-        
+        print(f"Este carro está rodando a {self.odometer_reading}Km/h.")
+    
     def update_odometer(self, mileage):
-        '''Define a leitura do hodômetro para o valor fornecido'''
         if mileage >= self.odometer_reading:
             self.odometer_reading = mileage
         else:
-            print('Não é possível reverter o hodômetro.')
-            
+            print("Não é possível alterar")
+        
     def increment_odometer(self, kilometers):
         self.odometer_reading += kilometers
+
+class ElectricCar(Car):
+    '''Representa aspectos específicos de carros elétricos'''
+    def __init__(self, make, model, year):
+        super().__init__(make, model, year)
+        self.battery_size = 70
+    
+    def describe_battery(self):
+        '''Descreve a capacidade da bateria'''
+        print(f"Esse carro possui {self.battery_size}kWh bateria")
         
-my_car = Car('GM', 'Onix', 2013)
-print(my_car.get_descriptive_name())
+    def fill_gas_tank(self):
+        '''Carros elétricos não têm tanques de gasolina'''
+        print("Esse carro não precisa de tanque de gasolina.")
 
-my_car.update_odometer(23)
-my_car.read_odometer()
-
-my_car.increment_odometer(20)
-my_car.read_odometer()
+my_tesla = ElectricCar('tesla', 'model s', 2016)
+my_tesla.get_descritive_name()
+my_tesla.describe_battery()
+my_tesla.fill_gas_tank()
